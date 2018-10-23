88c929409ea9339c7c609f18fe52226793f21734
phrlog <kh@linkprofit.com>
2018-10-23 12:49:32 +0300

    небольшие правки

--- tests/Strategies/mt_rand.php
+++ tests/Strategies/mt_rand.php
@@ -6,7 +6,9 @@ function mt_rand($min, $max)
 {
     if ($min === 1 && $max === 10) {
         return 3;
-    } elseif ($min === 1 && $max === 3) {
+    }
+
+    if ($min === 1 && $max === 3) {
         return 3;
     }
 }
